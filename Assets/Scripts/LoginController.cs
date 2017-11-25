41bf43fcdace80891efe75700f0031cfb786cba7
최수민 <choisoomin@choesumin-ui-MacBook-Pro.local>
2017-11-25 18:19:22 +0900

    done

--- Assets/Scripts/LoginController.cs
+++ Assets/Scripts/LoginController.cs
@@ -1,60 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Facebook.Unity;
+using UnityEngine.SceneManagement;
 
-public class LoginController : MonoBehaviour {
+public class LoginController : MonoBehaviour 
+{
 
 	// Use this for initialization
-	void Start () {
-
+	void Start () 
+	{
+		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 	}
 
-	public void LoginFacebook(){
-		
-//		FB.Init ( delegate() {
-//			
-//			FB.LogInWithReadPermissions(
-//
-//				new List<string>(){"public_profile", "email", "user_friends"},
-//
-//				delegate(ILoginResult result) {
-//				
-//					if(result.Error == null){
-//
-//						LoadFacebookMe();
-//
-//						//Application.LoadLevel("Game");
-//					}
-//
-//					Debug.Log(result.RawResult);
-//
-//				});
-//			
-//		});
+	public void MoveGame()
+	{
+		SceneManager.LoadScene ("Game");
 	}
-
-	public void LoadFacebookMe(){
-//		FB.API ("/me", HttpMethod.GET, delegate(IGraphResult result) {
-//			Debug.Log(result.RawResult);
-//			Debug.Log(result.ResultDictionary["name"]);
-//			Debug.Log(result.ResultDictionary["id"]);
-//			Debug.Log("https://graph.facebook.com/" + result.ResultDictionary["id"] + "/picture");
-//			LoadFacebookFriends();
-//		});
-	}
-
-	public void LoadFacebookFriends(){
-	
-//		FB.API ("/me/friends", HttpMethod.GET, delegate(IGraphResult result) {
-//			Debug.Log(result.RawResult);
-//		});
-
-	}
-
 }
